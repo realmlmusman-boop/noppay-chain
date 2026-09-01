c93f49ffe202c20b56ff5a0297edec8ddc04dca9
realmlmusman-boop <realmlmusman@gmail.com>
2026-09-01 10:43:36 +0000

    Connect storage node to blockchain persistence

--- node/storage-node.ts
+++ node/storage-node.ts
@@ -1,50 +1,54 @@
-import { BlockchainStorage } from "../storage/blockchain-storage.js";
+import {
+    BlockchainStorage,
+    type StoredBlock,
+} from "../storage/blockchain-storage.js";
 import { Blockchain } from "../core/blockchain.js";
 import type { Block } from "../core/block-builder.js";
-import type { Transaction } from "../core/types.js";
 
 export class StorageNode {
-  readonly blockchain: Blockchain;
+    readonly blockchain: Blockchain;
     readonly storage: BlockchainStorage;
 
-      constructor(filePath: string) {
-          this.blockchain = new Blockchain();
-              this.storage = new BlockchainStorage(filePath);
-                }
+    constructor(filePath: string) {
+        this.blockchain = new Blockchain();
+        this.storage = new BlockchainStorage(filePath);
+    }
 
-                  async load(): Promise<void> {
-                      const storedBlocks = await this.storage.load();
+    async load(): Promise<void> {
+        const storedBlocks = await this.storage.load();
 
-                          if (storedBlocks.length === 0) {
-                                return;
-                                    }
+        if (storedBlocks.length === 0) {
+            return;
+        }
 
-                                        const blocks: Block[] = storedBlocks.map((block) => ({
-                                              index: block.index,
-                                                    previousHash: block.previousHash,
-                                                          timestamp: block.timestamp,
-                                                                transactions: block.transactions as Transaction[],
-                                                                      miner: block.miner,
-                                                                            reward: BigInt(block.reward),
-                                                                                  nonce: block.nonce,
-                                                                                        hash: block.hash,
-                                                                                            }));
+        const blocks: Block[] = storedBlocks.map((stored): Block => ({
+            index: stored.index,
+            previousHash: stored.previousHash,
+            timestamp: stored.timestamp,
+            transactions: stored.transactions as Block["transactions"],
+            miner: stored.miner,
+            reward: BigInt(stored.reward),
+            nonce: stored.nonce,
+            hash: stored.hash,
+        }));
 
-                                                                                                this.blockchain.replaceBlocks(blocks);
-                                                                                                  }
+        this.blockchain.replaceBlocks(blocks);
+    }
 
-                                                                                                    async save(): Promise<void> {
-                                                                                                        const blocks = this.blockchain.blocks.map((block) => ({
-                                                                                                              index: block.index,
-                                                                                                                    previousHash: block.previousHash,
-                                                                                                                          timestamp: block.timestamp,
-                                                                                                                                transactions: block.transactions,
-                                                                                                                                      miner: block.miner,
-                                                                                                                                            reward: block.reward.toString(),
-                                                                                                                                                  nonce: block.nonce,
-                                                                                                                                                        hash: block.hash,
-                                                                                                                                                            }));
+    async save(): Promise<void> {
+        const blocks: readonly StoredBlock[] = this.blockchain.blocks.map(
+            (block: Block): StoredBlock => ({
+                index: block.index,
+                previousHash: block.previousHash,
+                timestamp: block.timestamp,
+                transactions: block.transactions,
+                miner: block.miner,
+                reward: block.reward.toString(),
+                nonce: block.nonce,
+                hash: block.hash,
+            }),
+        );
 
-                                                                                                                                                                await this.storage.save(blocks);
-                                                                                                                                                                  }
-                                                                                                                                                                  }
+        await this.storage.save(blocks);
+    }
+}
