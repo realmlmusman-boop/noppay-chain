6ca2c90bf37c3582a90bcfd05d1ed2b6e3fd3df1
realmlmusman-boop <realmlmusman@gmail.com>
2026-09-04 21:58:15 +0000

    Validate wallet addresses

--- wallet/wallet-service.ts
+++ wallet/wallet-service.ts
@@ -3,3 +3,7 @@ import { createWallet, type Wallet } from "./wallet.js";
 export function createNewWallet(): Wallet {
   return createWallet();
 }
+
+export function isValidWalletAddress(address: string): boolean {
+  return address.startsWith("NPP") && address.length === 27;
+}
